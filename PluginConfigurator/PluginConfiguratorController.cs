0a08ffe9c17f3b8afc45fb45150dfdb896ff5b28
eternalUnion <dnzbyk12345@gmail.com>
2023-12-22 21:13:58 +0300

    Fixed typo

--- PluginConfigurator/PluginConfiguratorController.cs
+++ PluginConfigurator/PluginConfiguratorController.cs
@@ -649,7 +649,7 @@ namespace PluginConfig
 			config = PluginConfigurator.Create("Plugin Configurator", PLUGIN_GUID);
 			config.SetIconWithURL(Path.Combine(workingDir, "icon.png"));
 
-			ButtonArrayField binButtons = new ButtonArrayField(config.rootPanel, "binButtons", 2, new float[] { 0.5f, 0.5f }, new string[] { "Deleted Presets Folder", "Reset Presests Folder" });
+			ButtonArrayField binButtons = new ButtonArrayField(config.rootPanel, "binButtons", 2, new float[] { 0.5f, 0.5f }, new string[] { "Deleted Presets Folder", "Reset Presets Folder" });
 			binButtons.OnClickEventHandler(0).onClick += () => Application.OpenURL(PresetOldFileManager.trashBinDir);
 			binButtons.OnClickEventHandler(1).onClick += () => Application.OpenURL(PresetOldFileManager.resetBinDir);
 			new ConfigHeader(config.rootPanel, "Patches").textColor = new Color(250 / 255f, 160 / 255f, 160 / 255f);
